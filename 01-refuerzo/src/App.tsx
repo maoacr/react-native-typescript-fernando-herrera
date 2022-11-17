19656730a04f4266f2c58074f16a9fcf74e557cc
Mao <maoacr@gmail.com>
2022-11-17 15:45:26 -0500

    Sync 01-refuerzo folder

---
+++ 01-refuerzo/src/App.tsx
@@ -0,0 +1,13 @@
+import TiposBasicos from "./typescript/TiposBasicos";
+
+const App = () => {
+  return (
+    <div className='mt-2'> 
+      <h1>Introducción a TypeScript y React</h1>
+      <hr />
+      <TiposBasicos />
+    </div>
+  )
+}
+
+export default App;
---
+++ 01-refuerzo/src/typescript/TiposBasicos.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TiposBasicos = () => {
+
+  let nombre = 'Mario';
+
+  return (
+    <div>
+      <h3>Tipos básicos</h3>
+      {nombre}
+    </div>
+  )
+}
+
+export default TiposBasicos
